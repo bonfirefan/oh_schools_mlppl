9ac9e4cad7b827303d31b9abf7dcbe964bb9df1d
bonfirefan <bonnie.y.fan@gmail.com>
2020-01-30 15:53:17 -0500

    added csv file arg

--- setup_local.sh
+++ setup_local.sh
@@ -1,5 +1,5 @@
 
 head -n 1000 'assignment1/test.csv' | tr [:upper:] [:lower:] | tr ' ' '_' | sed 's/#/num/' | csvsql -i postgresql --db-schema ohschool --tables ohcensus > 'table_script.sql'
 psql -f setup_sql.sql
-python clean_sql.py 'table_script.sql' 'table_script_cleaned.sql'
+python clean_sql.py 'table_script.sql' 'table_script_cleaned.sql' 'assignment1/test.csv'
 psql -f table_script_cleaned.sql
