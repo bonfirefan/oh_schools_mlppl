9fc8f72e7dcd35cf11a00cbb806f351d8fb7cc7a
bonfirefan <bonnie.y.fan@gmail.com>
2020-01-30 11:30:47 -0500

    census

--- assignment1/census_load.py
+++ assignment1/census_load.py
@@ -1,13 +1,27 @@
 import censusdata
+import pandas as pd
 
-MY_API_KEY = '1936736258d353a204c8a1f6189f327836630bbc'
+#function to make list of all county ids in state (given by census state id)
+def county_list(state_number):
+    counties = censusdata.geographies(censusdata.censusgeo([('state', state_number),('county','*')]), 'acs5', 2018)
+    county_list = []
+    for i in counties.keys():
+        county_list.append(counties[i].geo[1][1])
+    return county_list
 
-# Find states
-censusdata.geographies(censusdata.censusgeo([('state', '*')]), 'acs5', 2015)
+#function to pull defined variables for blocks in specified state, looping over countties
+#(input state id and list of variables)
+def block_pull(state_id,variable_list):
+    c_list = county_list(state_id)
+    for i in range(0,len(c_list)):
+        geo = censusdata.censusgeo([('state',state_id),('county',c_list[i]),('tract','*'),('block group','*')])
+        county_df = censusdata.download('acs5',2018,geo,variable_list)
+        if i == 0:
+            data = county_df
+        else:
+            data = pd.concat([data,county_df])
+    return data
 
-# Find sample colum
-ohbg = censusdata.download('acs5', 2015,
-                    censusdata.censusgeo([('state', '30'), ('county', '*'), ('block group', '*')]),
-                        ['B23025_003E'])
-print(ohbg)
-ohbg.to_csv('ohbg_test.csv')
+variables_list = ['B02001_001E']
+county = block_pull('39',variables_list)
+count.to_csv('test_upload.csv')
